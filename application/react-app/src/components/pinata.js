066c61709e32d0ea62a0752eef96b26b4c40cc15
jerem <jeremy.beaugeard@isen-ouest.yncrea.fr>
2021-04-15 17:09:01 +0200

    add mint nft

--- application/react-app/src/components/pinata.js
+++ application/react-app/src/components/pinata.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
-require('dotenv').config({path: '.env'})
 
 const pinataApiKey = "027a3a5204bf3ff59ffc"
 const pinataSecretApiKey = "b8c59aef2458bb2f4015b6f2624e32fb9eab4c1635d5c1fcd2336b32646453f3"
