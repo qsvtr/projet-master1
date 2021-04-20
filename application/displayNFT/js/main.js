09bd928392f3c60a394f592b77b6b553d6333328
qsvtr <quentin.sauvetre@protonmail.com>
2021-04-20 23:09:19 +0200

    feat: static website to display diploma

--- application/displayNFT/js/main.js
+++ application/displayNFT/js/main.js
@@ -1,8 +1,5 @@
-
-
 window.addEventListener('load', async () => {
     const params = getUrlVars();
-    console.log(params)
     if (!params.id || !params.key) {
         console.log('verify url parameters')
         $('#error').html('verify url parameters')
--- application/displayNFT/js/utils.js
+++ application/displayNFT/js/utils.js
@@ -4,11 +4,27 @@ const getUrlVars = () => {
 }
 
 const getToken = async (id, key) => {
-    fetch("http://localhost:8080/api/getNFT", {method: "POST", body: JSON.stringify({id: id, key: key})})
-        .then(res => {
-            console.log("res:", res);
+    $('#loading').html(`<p>loading...</p>`)
+    fetch("http://localhost:8080/api/getNFT", {method: "POST", headers: new Headers({'content-type': 'application/json'}), body: JSON.stringify( {id: id, key: key} )})
+        .then(res => res.json())
+        .then(data => {
+            $('#loading').html('')
+            const metadata = data.data
+            console.log(data);
+            $('#qrcodeText').html("Scan another!")
+            if(data.message !== 'success') {
+                $('#error').append(`<h2 style="color: red">${data.message}</h2>`)
+            } else {
+                $('#content').append('<div class="alert alert-success"><strong>Success!</strong> this diploma is authentic!</div><br>')
+                $('#content').append(`<h2 style="">${metadata.name}</h2>`)
+                $('#content').append(`<img class="logo" style="height: auto;" src=${metadata.image} alt='logo'/>`)
+                $('#content').append(`<h2 style="">${metadata.description}</h2>`)
+                $('#content').append(`<p style=""><strong>${metadata.attributes.lastname}</strong> <strong>${metadata.attributes.firstname}</strong> born on <strong>${metadata.attributes.birthdate}</strong></p>`)
+            }
         })
         .catch(error => {
+            $('#loading').html('')
+            $('#error').append(`<h2 style="color: red">fatal error</h2>`)
             console.log('error', error)
         })
 }
